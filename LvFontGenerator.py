98b646830823061634210e6cc956a5875e95b208
Toubidou <email>
2026-01-20 15:52:03 +0800

    fix: 解决在‘--symbol’中不能输入空格的问题；fun: 增加判断是否有‘--symbol’选项的功能

--- LvFontGenerator.py
+++ LvFontGenerator.py
@@ -1,5 +1,5 @@
 import json
-import os
+import subprocess
 
 with open('config.json', 'r', encoding= 'utf-8') as f:
     try: 
@@ -7,10 +7,27 @@ with open('config.json', 'r', encoding= 'utf-8') as f:
     except json.JSONDecodeError:
         pass
 
+
 for font_cfg in fonts_config:
-    symbols = "".join(font_cfg["symbols"].split())
-    command = "lv_font_conv.cmd --no-compress --no-prefilter " + "--font " + font_cfg["font_path"] +\
-        " --size " + f'{font_cfg["size_pixel"]}' + " --format lvgl " + "--bpp " + f'{font_cfg["bpp"]}' +\
-        " --symbols " + symbols + " -o " + font_cfg["output"]
-    os.system(command)
-    # print(command)
+    if 'range' in font_cfg:
+        r = subprocess.run([
+            'lv_font_conv.cmd',
+            '--font', font_cfg["font_path"],
+            '--size', f'{font_cfg["size_pixel"]}',
+            '--format', 'lvgl',
+            '--bpp', f'{font_cfg["bpp"]}',
+            '--symbols', font_cfg["symbols"],
+            '--range', font_cfg["range"],
+            '-o', font_cfg["output"] 
+        ])
+    else:
+        r = subprocess.run([
+            'lv_font_conv.cmd',
+            '--font', font_cfg["font_path"],
+            '--size', f'{font_cfg["size_pixel"]}',
+            '--format', 'lvgl',
+            '--bpp', f'{font_cfg["bpp"]}',
+            '--symbols', font_cfg["symbols"],
+            '-o', font_cfg["output"] 
+        ])
+    # print(r)
